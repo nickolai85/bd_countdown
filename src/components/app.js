6ca744e43648cf2516c7cfe9838e48af2bac3c0d
Nicolai <organ.nicolai@gmail.com>
2018-08-01 22:13:48 +0300

    rendering JSX based on app state

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import Picker from './picker';
+import Button from './button';
+import Clock from './clock';
 
 export default class App extends Component {
-  render() {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      active: false
+    }
+  }
+  renderItems = function() {
+    if(this.state.active) {
+      return [
+        <Clock/>,
+
+      ]
+    } else {
+      return Button('Generate Countdown', () => this.setState({ active: true }))
+    }
+  }.bind(this)
+
+    render() {
+
+    // return <div className=""><Clock/></div>
+
     return (
-      <div>
-        <h1>DevCamp React Starter</h1>
+      <div className="grid">
+        <h1 className="grid__title">Birthday Countdown</h1>
+
+        <div className="grid__skew-dark-one-box"></div>
+        <div className="grid__skew-dark-two"></div>
+        <div className="grid__skew-dark-three"></div>
+
+        <div className="grid__skew-light-one"></div>
+        <div className="grid__skew-light-two"></div>
+        <div className="grid__skew-light-three-box"></div>
+
+        <Picker/>
+        { this.renderItems() }
+
       </div>
     );
   }
